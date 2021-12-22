4c8facd2dc981e37323da2ae852b4cb29c440a66
lss95 <tjdtn951007@gmaill.com>
2021-12-22 16:15:09 +0900

    mustache 적용중 ajax는 아직

---
+++ src/main/java/com/example/hello/web/IndexController.java
@@ -0,0 +1,23 @@
+package com.example.hello.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/input")
+    public String input() {
+        return "input";
+    }
+}
